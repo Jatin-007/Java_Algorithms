23230c7bfd068a79459abc81e9c5ac4cc30bfdf2
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-02-25 00:06:39 -0800

    BubbleSort

--- BubbleSort/src/JavaAlgorithms/BubbleSort/Main.java
+++ BubbleSort/src/JavaAlgorithms/BubbleSort/Main.java
@@ -3,11 +3,30 @@ package JavaAlgorithms.BubbleSort;
 public class Main {
 
     public static void main(String[] args) {
+        int[] intArray = {25, -15, 7, 20, 9, 55, 35};
 
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[i + 1]) {
+                    swap(intArray , i , i+ 1);
+                }
+            }
+        }
+        
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
     }
 
 
-    public void Swap() {
+    public static void swap(int arr[], int i, int j) {
+        if (i == j) {
+            return;
+        } else {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
 
     }
 }
