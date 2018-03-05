d4bd5efb1a627958123bbbc8bcbd505a2f3850d4
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-03-05 01:32:54 -0800

    MergeSort

---
+++ MergeSort/src/JavaAlgorithms/MergeSort/Main.java
@@ -0,0 +1,11 @@
+// MergeSort is a way of sorting an array in a more efficient way by splitting up the array into its sub arrays until
+// each array has only one element contained in it... eg-
+// An array of 7 will split up to 3 and 4 and later on to 1,1,1,1,1,1,1 each by calculating the midpoint !
+package JavaAlgorithms.MergeSort;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
