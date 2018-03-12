0bc72b631b1518b44efe4a976f6ad9f9f66b8a3e
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-03-11 19:01:05 -0700

    QuickSort

---
+++ CountingSort/src/JavaAlgorithms/CountingSort/Main.java
@@ -0,0 +1,16 @@
+package JavaAlgorithms.CountingSort;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] intArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+
+	    for(int i=0; i < intArray.length; i++){
+	        System.out.println(intArray[i]);
+        }
+    }
+
+    public static void countingSort(int[] input, int min, int max){
+
+    }
+}
