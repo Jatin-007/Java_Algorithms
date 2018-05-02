0f795d1cf981ae8e96edc6b3e645c5688cb3ff2f
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-05-02 01:55:16 -0700

    Lists

---
+++ Lists/ArrayList/ArrayList.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String args[]){
+    // Implementing List
+     List<Employee> employeeList = new ArrayList<>(); // syntax works for java1.6 and above
+
+  }
+}
---
+++ Lists/ArrayList/README.md
@@ -0,0 +1,2 @@
+#All About List
+This subdirectory will hold the algorithmic solutions to the most common List type algorithmic questions out there.
